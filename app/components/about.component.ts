4206911733060d8db0165c343f5d8edd52f2231c
jterrell <jterrell@drfirst.com>
2016-08-13 00:54:19 -0400

    Added logo and various CSS changes

--- app/components/about.component.ts
+++ app/components/about.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-hero-detail',
-  template: "Hello About Us!"
+  template: `<ul>
+  <li>Contact Information:</li>
+<li>704-200-4459</li>
+<li><a href="mailto: terrie.trainforlife@gmail.com">terrie.trainforlife@gmail.com</a></li>
+</ul>`
 })
 export class AboutComponent {
 }
